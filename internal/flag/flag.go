0df6c5d1185319f0e55ff64e45c47a935c613451
Andronik Maria <andronikmar@yandex.com>
2024-02-08 22:50:40 +0300

    iter4_10

--- internal/flag/flag.go
+++ internal/flag/flag.go
@@ -1,9 +1,9 @@
 package flag
 
 import (
-    "github.com/caarlos0/env/v6"
+    //"github.com/caarlos0/env/v6"
     "flag"
-    "os"
+    //"os"
 )
 var (
 	FlagRunAddr string
@@ -12,24 +12,24 @@ var (
 )
 
 
-type Config struct {
-    ReportInterval int64 `env:"REPORT_INTERVAL"`
-    PollInterval int64 `env:"POLL_INTERVAL"`
-}
+//type Config struct {
+    //ReportInterval int64 `env:"REPORT_INTERVAL"`
+    //PollInterval int64 `env:"POLL_INTERVAL"`
+//}
 
 func ParseFlags() {
-    var cfg Config
+    //var cfg Config
     flag.Int64Var(&ReportInterval, "r", 10, "time to sleep for report interval")
     flag.Int64Var(&PollInterval, "p", 2, "time to sleep for poll interval")
     flag.StringVar(&FlagRunAddr, "a", "localhost:8080", "address and port to run server")
     flag.Parse()
-    if envRunAddr := os.Getenv("ADDRESS"); envRunAddr != "" {
-        FlagRunAddr = envRunAddr
-    }
-    err := env.Parse(&cfg)
-    if err != nil {
-        panic(err)
-    }
+    //if envRunAddr := os.Getenv("ADDRESS"); envRunAddr != "" {
+        //FlagRunAddr = envRunAddr
+    //}
+    //err := env.Parse(&cfg)
+    //if err != nil {
+        //panic(err)
+    //}
 
 
 }
