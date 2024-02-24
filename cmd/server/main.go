0b6b216432e554cb09cfef7ec589dd7bb0e85afa
Andronik Maria <andronikmar@yandex.com>
2024-02-24 22:32:38 +0300

    iter6_1

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,15 +4,30 @@ import (
 	"net/http"
 
 	"github.com/andromaril/agent-smith/internal/flag"
+	logging "github.com/andromaril/agent-smith/internal/loger"
 	"github.com/andromaril/agent-smith/internal/server/handler"
 	"github.com/andromaril/agent-smith/internal/server/storage"
 	"github.com/go-chi/chi/v5"
+	"go.uber.org/zap"
 )
+
+var sugar zap.SugaredLogger
 
 func main() {
 	flag.ParseFlags()
+	logger, err1 := zap.NewDevelopment()
+	if err1 != nil {
+		panic(err1)
+	}
+	defer logger.Sync()
+	sugar = *logger.Sugar()
+	sugar.Infow(
+		"Starting server",
+		"addr", flag.FlagRunAddr,
+	)
 	newMetric := storage.NewMemStorage()
 	r := chi.NewRouter()
+	r.Use(logging.WithLogging(sugar))
 	r.Route("/value", func(r chi.Router) {
 		r.Get("/{pattern}/{name}", handler.GetMetric(newMetric))
 	})
@@ -21,8 +36,12 @@ func main() {
 		r.Post("/{pattern}/{name}/{value}", handler.GaugeandCounter(newMetric))
 	})
 	r.Get("/", handler.GetHTMLMetric(newMetric))
-	err := http.ListenAndServe(flag.FlagRunAddr, r)
-	if err != nil {
-		panic(err)
+	//err := http.ListenAndServe(flag.FlagRunAddr, r)
+	//if err != nil {
+	//panic(err)
+	//}
+	if err := http.ListenAndServe(flag.FlagRunAddr, r); err != nil {
+		// записываем в лог ошибку, если сервер не запустился
+		sugar.Fatalw(err.Error(), "event", "start server")
 	}
 }
---
+++ internal/loger/logging.go
@@ -0,0 +1,53 @@
+package logging
+
+import (
+	"net/http"
+	"time"
+
+	"go.uber.org/zap"
+)
+
+type (
+	// берём структуру для хранения сведений об ответе
+	responseData struct {
+		status int
+		size   int
+	}
+
+	// добавляем реализацию http.ResponseWriter
+	loggingResponseWriter struct {
+		http.ResponseWriter // встраиваем оригинальный http.ResponseWriter
+		responseData        *responseData
+	}
+)
+
+var sugar zap.SugaredLogger
+
+func WithLogging(sugar zap.SugaredLogger) func(http.Handler) http.Handler {
+	return func(h http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
+		responseData := &responseData{
+			status: 0,
+			size:   0,
+		}
+		lw := loggingResponseWriter{
+			ResponseWriter: w, // встраиваем оригинальный http.ResponseWriter
+			responseData:   responseData,
+		}
+		h.ServeHTTP(&lw, r) // внедряем реализацию http.ResponseWriter
+
+		duration := time.Since(start)
+
+		sugar.Infoln(
+			"uri", r.RequestURI,
+			"method", r.Method,
+			"status", responseData.status, // получаем перехваченный код статуса ответа
+			"duration", duration,
+			"size", responseData.size, // получаем перехваченный размер ответа
+		)
+	})
+}
+	//return http.HandlerFunc(logFn)
+}
