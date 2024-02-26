a9a02c39f4f00bf5868b58b0bfec200df9ed877d
Andronik Maria <andronikmar@yandex.com>
2024-02-26 21:44:20 +0300

    iter_15

--- internal/server/handler/handler.go
+++ internal/server/handler/handler.go
@@ -15,6 +15,7 @@ import (
 func GetMetricJSON(m *storage.MemStorage) http.HandlerFunc {
 	return func(res http.ResponseWriter, req *http.Request) {
 		var r model.Metrics
+		res.Header().Set("Content-Type", "application/json")
 		dec := json.NewDecoder(req.Body)
 		if err := dec.Decode(&r); err != nil {
 			res.WriteHeader(http.StatusBadRequest)
@@ -73,6 +74,7 @@ func GaugeandCounterJSON(m *storage.MemStorage) http.HandlerFunc {
 				res.WriteHeader(http.StatusNotFound)
 				return
 			}
+			res.Header().Set("Content-Type", "application/json")
 			res.WriteHeader(http.StatusOK)
 			resp := model.Metrics{
 				ID:    r.ID,
@@ -97,6 +99,7 @@ func GaugeandCounterJSON(m *storage.MemStorage) http.HandlerFunc {
 				res.WriteHeader(http.StatusNotFound)
 				return
 			}
+			res.Header().Set("Content-Type", "application/json")
 			res.WriteHeader(http.StatusOK)
 			resp := model.Metrics{
 				ID:    r.ID,
@@ -106,7 +109,7 @@ func GaugeandCounterJSON(m *storage.MemStorage) http.HandlerFunc {
 			enc := json.NewEncoder(res)
 			if err := enc.Encode(resp); err != nil {
 				return
-			}else {
+			} else {
 				res.WriteHeader(http.StatusNotFound)
 			}
 		}
