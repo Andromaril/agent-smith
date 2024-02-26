c8110787d9e90383117b9c8be2ebfe954efa50f1
Andronik Maria <andronikmar@yandex.com>
2024-02-26 23:01:40 +0300

    iter_17

--- internal/model/model.go
+++ internal/model/model.go
@@ -2,7 +2,7 @@ package model
 
 type Metrics struct {
 	ID    string   `json:"id"`              // имя метрики
-	MType string   `json:"Mtype"`            // параметр, принимающий значение gauge или counter
+	MType string   `json:"mtype"`           // параметр, принимающий значение gauge или counter
 	Delta *int64   `json:"delta,omitempty"` // значение метрики в случае передачи counter
 	Value *float64 `json:"value,omitempty"` // значение метрики в случае передачи gauge
- }
+}
