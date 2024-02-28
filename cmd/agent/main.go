76f974e20da169d2caebe6867ad50bbdd5b74d14
Andronik Maria <andronikmar@yandex.com>
2024-02-28 21:05:25 +0300

    iter7_45

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -7,6 +7,8 @@ import (
 	"github.com/andromaril/agent-smith/internal/agent/creator"
 	"github.com/andromaril/agent-smith/internal/agent/metric"
 	"github.com/andromaril/agent-smith/internal/flag"
+	"github.com/go-resty/resty/v2"
+	"go.uber.org/zap"
 )
 
 func UpdateMetric() {
@@ -20,6 +22,8 @@ func UpdateMetric() {
 func main() {
 	flag.ParseFlags()
 	var i int64
+	client := resty.New()
+	var sugar zap.SugaredLogger
 	for {
 		time.Sleep(time.Second)
 		i++
@@ -29,7 +33,7 @@ func main() {
 			//i = i + flag.PollInterval
 		}
 		if i%flag.ReportInterval == 0 {
-			err := metric.SendAllMetricJSON2()
+			err := metric.SendAllMetricJSON2(sugar, client)
 			if err != nil {
 				panic(err)
 			}
--- internal/agent/metric/metric.go
+++ internal/agent/metric/metric.go
@@ -8,6 +8,7 @@ import (
 	"github.com/andromaril/agent-smith/internal/flag"
 	"github.com/andromaril/agent-smith/internal/model"
 	"github.com/go-resty/resty/v2"
+	"go.uber.org/zap"
 )
 
 // func SendGaugeMetric(name string, value float64) {
@@ -20,20 +21,28 @@ import (
 // 	}
 // }
 
-// func SendMetricJSON(res *model.Metrics) {
-// 	client := resty.New()
-// 	jsonData, err := json.Marshal(res)
+func SendMetricJSON(sugar zap.SugaredLogger, res model.Metrics, client *resty.Client, url string) {
+	//client := resty.New()
+	// //jsonData, err := json.Marshal(res)
 
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	url := fmt.Sprintf("http://%s/update/", flag.FlagRunAddr)
-// 	//fmt.Print(url)
-// 	_, err1 := client.R().SetHeader("Content-Type", "application/json").SetBody(jsonData).Post(url)
-// 	if err1 != nil {
-// 		panic(err)
-// 	}
-// }
+	// if err != nil {
+	// 	panic(err)
+	// }
+	//url := fmt.Sprintf("http://%s/update/", flag.FlagRunAddr)
+	//fmt.Print(url)
+	jsonData, err := json.Marshal(res)
+	if err != nil {
+		sugar.Errorw("JSON marshaling failed", err)
+	}
+
+	if err != nil {
+		panic(err)
+	}
+	_, err1 := client.R().SetHeader("Content-Type", "application/json").SetBody(jsonData).Post(url)
+	if err1 != nil {
+		panic(err)
+	}
+}
 
 // func SendCounterMetric(name string, value int64) {
 // 	client := resty.New()
@@ -79,10 +88,10 @@ import (
 // 	return nil
 // }
 
-func SendAllMetricJSON2() error {
+func SendAllMetricJSON2(sugar zap.SugaredLogger, client *resty.Client) error {
 	f := creator.CreateFloatMetric()
 	i := creator.CreateIntMetric()
-	client := resty.New()
+	//client := resty.New()
 	url := fmt.Sprintf("http://%s/update/", flag.FlagRunAddr)
 	for key, value := range f {
 		resp := model.Metrics{
@@ -90,15 +99,16 @@ func SendAllMetricJSON2() error {
 			MType: "gauge",
 			Value: &value,
 		}
-		jsonData, err := json.Marshal(resp)
+		// jsonData, err := json.Marshal(resp)
 
-		if err != nil {
-			panic(err)
-		}
-		_, err1 := client.R().SetBody(jsonData).SetHeader("Content-Type", "application/json").Post(url)
-		if err1 != nil {
-			panic(err1)
-		}
+		// if err != nil {
+		// 	panic(err)
+		// }
+		SendMetricJSON(sugar, resp, client, url)
+		// _, err1 := client.R().SetBody(jsonData).SetHeader("Content-Type", "application/json").Post(url)
+		// if err1 != nil {
+		// 	panic(err1)
+		// }
 	}
 	for key, value := range i {
 		resp := model.Metrics{
@@ -106,15 +116,16 @@ func SendAllMetricJSON2() error {
 			MType: "counter",
 			Delta: &value,
 		}
-		jsonData, err := json.Marshal(resp)
+		// jsonData, err := json.Marshal(resp)
 
-		if err != nil {
-			panic(err)
-		}
-		_, err1 := client.R().SetBody(jsonData).SetHeader("Content-Type", "application/json").Post(url)
-		if err1 != nil {
-			panic(err1)
-		}
+		// if err != nil {
+		// 	panic(err)
+		// }
+		// _, err1 := client.R().SetBody(jsonData).SetHeader("Content-Type", "application/json").Post(url)
+		// if err1 != nil {
+		// 	panic(err1)
+		// }
+		SendMetricJSON(sugar, resp, client, url)
 	}
 	return nil
 }
