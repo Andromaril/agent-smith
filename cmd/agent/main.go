1d7a40701327dd0e88ce5fdfcfd8903d860b6bdb
Andronik Maria <andronikmar@yandex.com>
2024-02-28 23:21:27 +0300

    iter7_3

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -21,32 +21,32 @@ func main() {
 	flag.ParseFlags()
 	var i int64
 	//client := resty.New()
-	// var t1 bool
-	// var t2 bool
-	// t1 = true
-	// t2 = false
+	var t1 bool
+	var t2 bool
+	t1 = true
+	t2 = false
 	//
 	//
 	for i = 0; ; i++ {
 		time.Sleep(time.Second)
 		//i++
-		if i%flag.PollInterval == 0 {
+		if t1&&i%flag.PollInterval == 0 {
 			creator.PollCount++
 			creator.RandomValue = rand.Float64()
 			//i = i + flag.PollInterval
-			// t2 = true
-			// t1 = false
+			t2 = true
+			t1 = false
 			// continue
 			time.Sleep(time.Second * time.Duration(flag.PollInterval))
 			
 		}
-		if i%flag.ReportInterval == 0 {
+		if t2&&i%flag.ReportInterval == 0 {
 			err := metric.SendAllMetricJSON2()
 			if err != nil {
 				panic(err)
 			}
-			// t1 = true
-			// t2 = false
+			t1 = true
+			t2 = false
 			// continue
 			//i = i + flag.ReportInterval
 			time.Sleep(time.Second * time.Duration(flag.ReportInterval))
