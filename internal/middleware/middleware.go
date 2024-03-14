343a732e6bbfc70b6115b40d1363c56b1e4f993e
Andronik Maria <andronikmar@yandex.com>
2024-03-14 23:30:07 +0300

    iter9_45

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -13,29 +13,31 @@ func GzipMiddleware(h http.Handler) http.Handler {
 		acceptEncoding := r.Header.Get("Accept-Encoding")
 		supportsGzip := strings.Contains(acceptEncoding, "gzip")
 		contentType := r.Header.Get("Content-Type")
-		accept := r.Header.Get("Accept")
+		//accept := r.Header.Get("Accept")
 		support := strings.Contains(contentType, "application/json")
 		support2 := strings.Contains(contentType, "text/html")
-		support3 := strings.Contains(accept, "html/text")
-		if support {
-			//По заданию мы проверяем не только Accept-Encoding, но и Content-Type для принятия решения о сжатии ответа.
-			if supportsGzip {
-				cw := gzip.NewCompressWriter(w)
-				ow = cw
-				ow.Header().Set("Content-Encoding", "gzip")
-				defer cw.Close()
-			}
+		//support3 := strings.Contains(accept, "html/text")
+		//if support {
+		//По заданию мы проверяем не только Accept-Encoding, но и Content-Type для принятия решения о сжатии ответа.
+		if supportsGzip {
+			cw := gzip.NewCompressWriter(w)
+			ow = cw
+			ow.Header().Set("Content-Encoding", "gzip")
+			defer cw.Close()
 		}
+		//}
 		contentEncoding := r.Header.Get("Content-Encoding")
 		sendsGzip := strings.Contains(contentEncoding, "gzip")
-		if sendsGzip || support2 || support3 {
-			cr, err := gzip.NewCompressReader(r.Body)
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				return
+		if support || support2 {
+			if sendsGzip {
+				cr, err := gzip.NewCompressReader(r.Body)
+				if err != nil {
+					w.WriteHeader(http.StatusInternalServerError)
+					return
+				}
+				r.Body = cr
+				defer cr.Close()
 			}
-			r.Body = cr
-			defer cr.Close()
 		}
 		h.ServeHTTP(ow, r)
 	})
