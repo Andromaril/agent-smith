05d437baa660ee402964c6ef338a644c73012ac2
Andronik Maria <andronikmar@yandex.com>
2024-03-15 01:26:46 +0300

    iter9_52

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -15,18 +15,18 @@ func GzipMiddleware(h http.Handler) http.Handler {
 		supportsGzip := strings.Contains(acceptEncoding, "gzip")
 		contentType := ow.Header().Values("Content-Type")
 		//accept := r.Header.Get("Accept")
-		support := slices.Contains(contentType, "application/json")
-		support2 := slices.Contains(contentType, "text/html")
+		//support := slices.Contains(contentType, "application/json")
+		//support2 := slices.Contains(contentType, "text/html")
 		//support3 := strings.Contains(accept, "html/text")
 		//if support || support2 {
 			//По заданию мы проверяем не только Accept-Encoding, но и Content-Type для принятия решения о сжатии ответа.
 			if supportsGzip {
-				if support || support2 {
+				//if support || support2 {
 				cw := gzip.NewCompressWriter(w)
 				ow = cw
 				ow.Header().Set("Content-Encoding", "gzip")
 				defer cw.Close()
-			}
+			//}
 		}
 		contentEncoding := r.Header.Get("Content-Encoding")
 		sendsGzip := strings.Contains(contentEncoding, "gzip")
