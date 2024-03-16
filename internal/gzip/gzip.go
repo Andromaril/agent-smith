17d307c8cb88a02fa814f2068cd2bc7f33e6bf68
Andronik Maria <andronikmar@yandex.com>
2024-03-16 13:14:46 +0300

    iter9_96

--- internal/gzip/gzip.go
+++ internal/gzip/gzip.go
@@ -41,10 +41,10 @@ func (c *compressWriter) Write(p []byte) (int, error) {
 }
 
 func (c *compressWriter) WriteHeader(statusCode int) {
-	if statusCode < 300 {
-		c.w.Header().Set("Content-Encoding", "gzip")
-	}
-	//c.w.WriteHeader(statusCode)
+	// if statusCode < 300 {
+	// 	c.w.Header().Set("Content-Encoding", "gzip")
+	// }
+	c.w.WriteHeader(statusCode)
 }
 
 func (c *compressWriter) Close() error {
