ade55635f0aeca3f2126dd0885f064fd70e3d8cc
Andronik Maria <andronikmar@yandex.com>
2024-03-24 20:04:56 +0300

    iter13_8

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"net/http"
 	"time"
 
 	_ "github.com/jackc/pgx/v5/stdlib"
 
+	logging "github.com/andromaril/agent-smith/internal/loger"
+	"github.com/andromaril/agent-smith/internal/middleware"
+	"github.com/andromaril/agent-smith/internal/server/handler"
 	"github.com/andromaril/agent-smith/internal/server/start"
-	"github.com/andromaril/agent-smith/internal/server/start/router"
+	"github.com/andromaril/agent-smith/internal/server/storage/storagedb"
 	"github.com/andromaril/agent-smith/internal/serverflag"
+	"github.com/go-chi/chi/v5"
 	"go.uber.org/zap"
 )
 
@@ -30,24 +35,23 @@ func main() {
 		newMetric.Load(serverflag.FileStoragePath)
 	}
 	defer db.Close()
-	//r := chi.NewRouter()
-	// r.Use(middleware.GzipMiddleware)
-	// r.Use(logging.WithLogging(sugar))
-	// r.Route("/value", func(r chi.Router) {
-	// 	r.Post("/", handler.GetMetricJSON(newMetric))
-	// 	r.Get("/{pattern}/{name}", handler.GetMetric(newMetric))
-	// })
+	r := chi.NewRouter()
+	r.Use(middleware.GzipMiddleware)
+	r.Use(logging.WithLogging(sugar))
+	r.Route("/value", func(r chi.Router) {
+		r.Post("/", handler.GetMetricJSON(newMetric))
+		r.Get("/{pattern}/{name}", handler.GetMetric(newMetric))
+	})
 
-	// r.Route("/update", func(r chi.Router) {
-	// 	r.Post("/", handler.GaugeandCounterJSON(newMetric))
-	// 	r.Post("/{pattern}/{name}/{value}", handler.GaugeandCounter(newMetric))
-	// })
-	// r.Get("/", handler.GetHTMLMetric(newMetric))
-	// r.Get("/ping", handler.Ping(newMetric.(storagedb.Interface)))
-	// r.Route("/updates", func(r chi.Router) {
-	// 	r.Post("/", handler.Update(newMetric))
-	// })
-	router.Router(newMetric, sugar)
+	r.Route("/update", func(r chi.Router) {
+		r.Post("/", handler.GaugeandCounterJSON(newMetric))
+		r.Post("/{pattern}/{name}/{value}", handler.GaugeandCounter(newMetric))
+	})
+	r.Get("/", handler.GetHTMLMetric(newMetric))
+	r.Get("/ping", handler.Ping(newMetric.(storagedb.Interface)))
+	r.Route("/updates", func(r chi.Router) {
+		r.Post("/", handler.Update(newMetric))
+	})
 	if serverflag.StoreInterval != 0 {
 		go func() {
 			time.Sleep(time.Second * time.Duration(serverflag.StoreInterval))
@@ -55,8 +59,8 @@ func main() {
 		}()
 	}
 
-	// if err := http.ListenAndServe(serverflag.FlagRunAddr, r); err != nil {
-	// 	sugar.Fatalw(err.Error(), "event", "start server")
+	if err := http.ListenAndServe(serverflag.FlagRunAddr, r); err != nil {
+		sugar.Fatalw(err.Error(), "event", "start server")
 
-	// }
+	}
 }
